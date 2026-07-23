3e8ce81956ef527d576140269bd7c36ccd5607ef
Andre-StudioNerd <deco.luisfernandes@gmail.com>
2026-07-23 15:06:00 -0300

    Novo visual

---
+++ js/dinos-data.js
@@ -0,0 +1,161 @@
+const dinossauros = {
+  alossauro: {
+    nomeExibicao: "Alossauro",
+    imagem: "img/alossauros.jpg",
+    texto:
+      "Alossauro (Allosaurus - em inglês), que significa (Lagarto Diferente) é um espécie de Dinossauro Teropodes que viveu no período do Jurássico, era o maior carnivoro de seu período. Viveu junto com os Ceratossauros (na qual era rivais), mas, como o Alossauro era mais alto e evoluído, vencia dos Ceratossauros nas disputas. Suas espécies viveram em quase todos os continentes exceto: Antartica e Ásia. O Alossauro media no máximo 14 metros de comprimento, 5,6 de altura e pesava o equivalente ao um elefante. Acreditam-se que caçavam em bandos, e, eram agéis e perigosos, mesmo sendo pesados. A cima de sua cabeça, possuía duas cristas que podiam servir como atrativo sexual. Sua boca era armada de dentes afiados e poderosos. Suas presas eram os Apatossauros, Camarassauros, Diplodocus, Estegossauros e outros dinossauros que os Alossauros caçavam (perseguindo esses bandos)",
+    nomeCientifico: "Allosaurus fragilis",
+    epoca: "Jurássico Superior",
+    alimentacao: "Carnívora",
+    tamanho: "Cerca de 4 metros de altura e 12 de comprimento",
+    peso: "de 1 á 3 toneladas",
+  },
+  trex: {
+    nomeExibicao: "Tiranossauro Rex",
+    imagem: "img/tiranorex.jpg",
+    texto:
+      "O Tiranossauro cujo nome significa lagarto tirano rei, foi um dos maiores carnívoros terrestres encontrados até hoje, perdendo apenas para o Giganotossauro e para o Carcharodontossauro, seus parentes, possivelmente mais agressivos, pois alguns cientistas acreditam que o Tiranossauro (Tyrannosaurus) era um carniceiro, assim como os atuais urubus. Seus dentes afiados eram ligeiramente curvos, de forma a agarrar melhor suas presas.Como o tubarão, uma vez que o Tiranossauro (Tyrannosaurus) abocanhava sua vítima, para escapar de seus dentes curvos era necessário entrar mais fundo em sua boca.Possuía cerca de cinqüenta desses dentes afiadíssimos de até 20 cm, uma perfeita máquina de fatiar carne. Suas vítimas prediletas eram os hadrossauros e ceratopsídeos.",
+    nomeCientifico: "Tyrannosaurus rex",
+    epoca: "Cretáceo Superior",
+    alimentacao: "Carnívora",
+    tamanho: "Cerca de 4 metros de altura e 12 metros de comprimento",
+    peso: "de 6 a 9 toneladas",
+  },
+  apatossauro: {
+    nomeExibicao: "Apatossauro",
+    imagem: "img/apatossauro.jpg",
+    texto:
+      "O Apatossauro cujo nome significa ” réptil bobo “, viveu durante o período Jurássico há cerca de 150 milhões de anos atrás, caracterizava-se por um corpo maciço, pescoço longo, cabeça pequena, pés amplos e a cauda muito comprida ( uma das mais compridas caudas do reino animal ). O longo pescoço, com cerca de 15 ossos grandes, era sustentado por músculos fortes. Tinha focinho longo, narinas no alto da cabeça e dentes em forma de cavilha. Foi descoberto primeiramente em 1877, no Colorado, EUA e também encontrado na Ásia Central e América do Sul. Com 90cm de comprimento, sua cabeça era minúscula em relação ao corpo. Isso fez com que ele ficasse conhecido como “réptil bobo” de cérebro pequeno. Sabe-se que o Apatossauro podia erguer-se sobre as patas traseiras devido a estas serem maiores e quando ele o fazia, sua cauda móvel lhe servia de apoio. Sua cauda era quase metade do comprimento ligeiramente mais afinada, parecida com um chicote. Devia usá-la para bater nos famintos carnívoros que rondavam os mais fracos do bando, ou seja, os filhotes e os mais velhos. Já o longo pescoço era usado como uma espécie de “observatório”, ajudando-o a detectar, a tempo, aproximação dos inimigos, bem como para alcançar as plantas mais altas das árvores. Para sustentar seu alto peso, as pernas do Apatossauro terminavam em pés amplos, como os do elefante. Os da frente eram equipados com uma garra no polegar, usada para chutar os inimigos. Devia passar grande parte do seu tempo comendo, pois necessitava ingerir diariamente entre 150 e 1000 quilos de alimentos. A dentição era fraca, e provavelmente não mastigava as plantas mais duras. Vivia em manadas e provavelmente os filhotes e as fêmeas andavam no meio, cercados pelos machos que formavam a proteção externa. Os fósseis foram descobertos pelo paleontólogo Othniel Charles Marsh em 1877 no Colorado, EUA. A descoberta consiste em dois esqueletos parciais que incluíam partes dos crânios.",
+    nomeCientifico: "Apatosaurus marsh",
+    epoca: "Jurássico Superior",
+    alimentacao: "Herbívora",
+    tamanho: "28 metros de comprimento e 13 metros de altura",
+    peso: "Cerca de 40 toneladas",
+  },
+  triceratops: {
+    nomeExibicao: "Triceratops",
+    imagem: "img/triceratops.jpg",
+    texto:
+      "O Triceratops era um DINOSSAURO do grupo dos CERATOPSÍDEOS que viveu há aproximadamente 66 milhões de anos atrás durante o período Cretáceo na América do Norte (EUA e Canadá), os quais possuíam a cabeça grande e extremamente forte, as maxilas laterais tinham bochechas musculosas, muitos dentes bastante afiados, um bico córneo curvado com o qual provavelmente arrancava as plantas duras, uma prolongação do crânio maciça que servia como escudo para proteção de eventuais ataques de predadores e combates entre machos e três chifres, sendo dois grandes acima dos olhos e um pequeno na ponta do nariz. Os Triceratops provavelmente vivessem em enormes bandos, os quais possuíam uma escala hierárquica com machos disputando posições e direitos de acasalamento, porém em situações de ataque de algum predador, acredita-se que se juntavam de forma defensiva contra o predador, formando uma “parede” de escudos e chifres contra o oponente, com os filhotes fracos e doentes no interior dessa “muralha” defensiva e dessa forma acredita-se que se defendiam contra o seu maior predador, o TIRANOSSAURO.",
+
+    nomeCientifico: "Triceratops horridus",
+    epoca: "Cretáceo",
+    alimentacao: "Herbívora",
+    tamanho: "10 metros de comprimento e 4 metros de altura",
+    peso: "Cerca de 6 toneladas",
+  },
+  estegossauro: {
+    nomeExibicao: "Estegossauro",
+    imagem: "img/estegossauro.jpg",
+    texto:
+      "O Estegossauro cujo nome significa “lagarto telhado” viveu há aproximadamente 200 milhões de anos atrás e recebeu esse nome porque pensava-se que suas placas ósseas protetoras eram dispostas como as telhas e um telhado. Hoje sabe-se que elas ficavam em pé ao longo da coluna vertebral, fixadas na pele duríssima e não no esqueleto. Há diversas teorias sobre a utilização dessas placas: como armadura ou talvez como reguladores térmicos, esquentando ou esfriando o corpo do Estegossauro conforme sua posição ao sol ou ao vento. Há também a possibilidade de estas placas terem sido usadas para a exibição, assim, quando um predador atacava ou um parceiro era encontrado, o Estegossauro bombeava sangue para as placas e elas tornavam-se mais vivas e vermelhas o que seduzia o parceiro ou amedrontava o atacante. O corpo desse animal era maciço e a cauda extremamente musculosa era a sua arma de ataque, já que contava com quatro espinhos ósseos de 50 centímetros a 1 metro de comprimento. O pescoço terminava numa cabeça absurdamente pequena, de apenas 40cm, comportando um cérebro do tamanho de uma noz. Quando o Estegossauro estava em perigo, curvava a cabeça para baixo, protegendo-a com sua nuca repleta de placas e ao mesmo tempo aplicava fortes golpes laterais com a cauda. Várias espécies de Estegossauro viveram na América do Norte. Alguns eram mais primitivos e tinham as placas menores e mais pontiagudas.",
+    nomeCientifico: "Stegosaurus armatus",
+    epoca: "Jurássico",
+    alimentacao: "Herbívora",
+    tamanho: "12 metros de comprimento e 4 metros de altura",
+    peso: "Cerca de 6 toneladas",
+  },
+
+  anquilossauro: {
+    nomeExibicao: "Anquilossauro",
+    imagem: "img/anquilossauro.jpg",
+    texto:
+      "Anquilossauro significa “lagarto fundido” em grego, e foi dado esse nome porque os ossos em seu crânio e outras partes do seu corpo foram fundidos, tornando o dinossauro extremamente robusto. Anquilossauro viveu no período cretáceo tardio, cerca de 65.5 a 66.8 milhões de anos atrás, e percorriam o oeste dos Estados Unidos e Alberta, Canadá. Ele provavelmente pesava até quatro toneladas. A parte superior do dinossauro era quase completamente coberto com armadura espessa que consiste em botões enormes e ovais de placas de osso, conhecidos como osteodermos ou escudos, que também são comuns em crocodilos, tatus e alguns lagartos. As placas, que variaram em tamanho, estavam alinhadas em fileiras horizontais regulares para baixo do pescoço do dinossauro, costas e quadris.",
+    nomeCientifico: "Ankilosaurus magniventris",
+    epoca: "Cretáceo",
+    alimentacao: "Herbívora",
+    tamanho: "10 metros de comprimento",
+    peso: "Cerca de 7 toneladas",
+  },
+  hadrossauro: {
+    nomeExibicao: "Hadrossauro",
+    imagem: "img/hadrossauro.jpg",
+    texto:
+      "O Hadrossauro era um dinossauro que possuía um bico córneo semelhante ao das tartarugas e ao das aves, por isso o nome significa lagarto com bico de pato, andavam em manadas para se protegerem, possuía dentes queixais e podiam andar com 4 ou 2 pernas. Com suas mandíbulas complexas, exclusivos e dentes perfeitamente adequado para rasgar e mastigar plantas, que floresceram na Terra na diversidade notável durante o período cretáceo tardio. Tão onipresente são seus restos que aprendemos mais sobre a paleobiologia e paleoecologia dos dinossauros dos Hadrossauros do que temos de qualquer outro grupo. Nos últimos anos, Hadrossauro dicou no centro das atenções. Pesquisadores de todo o mundo têm estudado novas espécimes e novos táxons que procuram expandir e clarificar o nosso conhecimento destes animais maravilhosos.",
+    nomeCientifico: "Hadrosaurus foulkii",
+    epoca: "Cretáceo",
+    alimentacao: "Herbívora",
+    tamanho: "13 metros de comprimento e 5 metros de altura",
+    peso: "Cerca de 7 toneladas",
+  },
+  braquiossauro: {
+    nomeExibicao: "Braquiossauro",
+    imagem: "img/braquiossauro.jpg",
+    texto:
+      "O Braquiossauro cujo nome significa (lagarto braço), foi descoberto em 1900 no Colorado, EUA, mas também viveu na Argélia e Tunísia.Esse animal provavelmente não poderia erguer-se nas patas traseiras como mostra o filme “Jurassic Park”, pois elas eram mais curtas que as dianteiras. Mesmo assim sua altura lhe permitia, sem esforço, comer as copas das árvores. A atividade principal do Braquiossauro era comer. O Braquiossauro passava a maior parte do dia comendo folhas de suas árvores prediletas como as coníferas (tipo pinheiros), cicadáceas e gingoáceas. Calcula-se que para abastecer seu corpanzil de até 90 toneladas (o mesmo que dezoito elefantes e mais que 2 BOENGS 747) comia mais ou menos 2 toneladas de plantas por dia. Apesar do grande peso, podia desenvolver uma velocidade de aproximadamente 20 km/h. Com certeza a terra devia tremer. Como as narinas desse animal ficavam no alto da cabeça, muitos cientistas acreditavam que ele vivia na água, comendo plantas aquáticas no fundo de lagos e rios. Nesse caso seu corpo ficaria debaixo da água e de vez em quando aparecia a pequena cabeça com as narinas através das quais ele respirava. Mas essa teoria foi superada, pois sabe-se que seus pulmões não teriam suportado a pressão da água.",
+    nomeCientifico: "Brachiossaurus brancai",
+    epoca: "Jurássico",
+    alimentacao: "Herbívora",
+    tamanho: "25 metros de comprimento e 15 metros de altura",
+    peso: "Cerca de 90 toneladas",
+  },
+  velociraptor: {
+    nomeExibicao: "Velociraptor",
+    imagem: "img/velociraptor.jpg",
+    texto:
+      "O Velociraptor cujo nome significa (ladrão veloz) vivia em grandes bandos, de 5 a 20 animais, era feroz e agressivo , alcançando altas velocidades, era uma das menores espécies de raptores. Esse predador percorria as florestas do período Cretáceo caçando mamíferos ou pequenos dinossauros herbívoros, matando-os e devorando-os. As vítimas ficavam aterrorizadas e tinham pouca chance de escapar. A longa e afiada garra existente em cada pata traseira chegava a 11cm e era voltada pra dentro e usada para ferir e dilacerar sua presa. Vivendo principalmente na Mongólia, o Velociraptor pertencia a uma família de perigosos e astutos caçadores. Esse animal tinha a aparência de um lagarto, tanto na pele quanto no formato, mas movia-se com a velocidade e a inteligência de um leopardo. Tinha uma cabeça de 16cm e focinho alongado. O Velociraptor possuía clavícula, o que era incomum nos outros dinossauro.",
+    nomeCientifico: "Velociraptor mongoliensis",
+    epoca: "Fim do Cretáceo",
+    alimentacao: "Carnívora",
+    tamanho: "2 metros de comprimento e 1 metro de altura",
+    peso: "Cerca de 80 quilos",
+  },
+  parassaurolofo: {
+    nomeExibicao: "Parassaurolofo",
+    imagem: "img/parassaurolofo.jpg",
+    texto:
+      "O Parassaurolofo cujo nome significa “paralelo ao Saurolofo”, recebeu esse nome porque viveu no mesmo período que o dinossauro Saurolophus. Pertencia a família dos Hadrossauros. Viveu principalmente nos EUA e Canadá e foi descoberto por volta de 1900. Talvez tenha sido o mais exótico dentre os chamados “dinossauro bico de pato”, devido a sua estranha crista em forma de um longo tubo ósseo, que começava nas narinas e se projetava por mais ou menos 1,6m além do crânio. Para alguns estudiosos, esse tubo era usados pelo animal para emitir som. Construindo um modelo da crista do Parassaurolofo, foi possível reproduzir o ruído que este dinossauro emitia e agora sabe-se que o que mais se ouvia nas florestas há 70 milhões de anos atrás, eram bramidos (som emitido pelos elefantes) profundos e vibrantes do Parassaurolofo, apelidado de “dinossauro trombeta”. Esse som poderia ter sido uma forma de chamamento dos machos para o acasalamento, ou um aviso de perigo, quando um predador se aproximava. Primeiramente achou-se que estes dinossauros habitavam os pântanos e as terras alagadiças, e só se alimentavam de plantas aquáticas. Sua crista, neste caso serviria como um tubo de respiração. Mas essa teoria foi superada, pois a crista, ao contrário de um tubo de respiração, não possuía abertura na extremidade. Viveu há aproximadamente 66 milhões de anos.",
+    epoca: "Cretáceo",
+    alimentacao: "Herbívora",
+    tamanho: "10 metros de comprimento e 8 metros de altura",
+    peso: "Cerca de 5 toneladas",
+  },
+  argentinossauro: {
+    nomeExibicao: "Argentinossauro",
+    imagem: "img/argentinossauro.jpg",
+    texto:
+      "Os Argentinossauros tinham vértebras muito rígidas. Sua flexibilidade foi sacrificada por causa do imenso peso. Eles viveram há aproximadamente 90 á 66 milhões de anos atrás, com maior intensidade de achados na Patagônia Argentina. Punham ovos do tamanho de uma bola de futebol, isto é, nasciam desproporcionalmente pequenos. Cientistas acreditam que aos 5 anos teria o tamanho de um pastor alemão, aos 15 teria o tamanho de um cavalo e só seriam adultos aos 50 anos. Supõe-se que tivessem sangue frio, pois com o sangue quente, teriam que ingerir 5 ou 6 toneladas de folhas por dia, uma quantidade teoricamente impossível para uma cabeça tão pequena. Mas com sangue frio necessitariam ingerir “apenas” 1 tonelada, mas isso é apenas uma teoria sobre os saurópodes e existem várias delas.",
+    epoca: "Cretáceo",
+    alimentacao: "Herbívora",
+    tamanho: "45 metros de comprimento e 20 metros de altura",
+    peso: "Cerca de 90 à 110 toneladas",
+  },
+  espinossauro: {
+    nomeExibicao: "Espinossauro",
+    imagem: "img/espinossauro.jpg",
+    texto:
+      "O Espinossauro cujo nome significa (Lagarto espinho), viveu no norte da África durante o período Cretáceo há aproximadamente 66 milhões de anos atrás e diferenciava-se dos outros dinossauros de sua família por uma espécie de “vela” nas costas. Essa “vela dorsal” era formada por prolongamentos das vértebras das colunas que mediam 1,8 metros de comprimento, cobertas de pele. Segundo os cientistas ela armazenava o calor do sol, dando-lhe a vantagem de ser mais ágil que os outros répteis. Esse animal possuía as tradicionais características dos outro predadores, a não ser os dentes que eram retos e não curvados e os braços um pouco maiores e mais fortes. Com o olhar frio e guloso, o Espinossauro (Spinosaurus) observava de longe um crocodilo. O aterrorizante dinossauro terópode, cujo esqueleto foi encontrado no Egito, agarrou o crocodilo de surpresa mordendo o seu pescoço e sacudindo-o como se fosse um simples peixinho. O indefeso animal nada pôde fazer contra aquele monstro de mais de 6 toneladas e acabava sendo devorado.",
+    epoca: "Cretáceo",
+    alimentacao: "Carnívora",
+    tamanho: "15 metros de comprimento e 6 metros de altura",
+    peso: "Cerca de 7 toneladas",
+  },
+  // Para adicionar novos dinossauros, basta colocar uma vírgula acima e copiar a estrutura!
+};
+
+// Captura o parâmetro 'id' da URL (Ex: detalhes.html?id=alossauro)
+const params = new URLSearchParams(window.location.search);
+const dinoId = params.get("id");
+
+if (dinoId && dinossauros[dinoId]) {
+  const dino = dinossauros[dinoId];
+
+  document.getElementById("page-title").innerText =
+    "Sitessauro - " + dino.nomeExibicao;
+  document.getElementById("dino-titulo").innerText = dino.nomeExibicao;
+  document.getElementById("dino-img").src = dino.imagem;
+  document.getElementById("dino-img").alt = dino.nomeExibicao;
+  document.getElementById("dino-texto").innerText = dino.texto;
+
+  document.getElementById("dino-nome-cientifico").innerText =
+    dino.nomeCientifico;
+  document.getElementById("dino-epoca").innerText = dino.epoca;
+  document.getElementById("dino-alimentacao").innerText = dino.alimentacao;
+  document.getElementById("dino-tamanho").innerText = dino.tamanho;
+  document.getElementById("dino-peso").innerText = dino.peso;
+} else {
+  document.getElementById("dino-titulo").innerText =
+    "Dinossauro não encontrado";
+  document.getElementById("dino-texto").innerText =
+    "Por favor, selecione um dinossauro na página inicial.";
+}
--- js/slider.js
+++ js/slider.js
@@ -1,38 +1,23 @@
 /*
-	Pega todos os elementos li da lista não ordenada
-	retornando um objeto NodeList, 
-	de acordo com posicionamento encontrado
-	no documento
+    Pega todos os elementos li da lista não ordenada
+    retornando um objeto NodeList
+*/
+var sliders = document.querySelectorAll("#sliders li");
 
-*/
-var sliders  = document.querySelectorAll('#sliders li');
-/*
- Define o indice, que será responsavel 
- por informar os slider atual
- */
-var current  = 0;
-/*
- Retorna o total de itens do slide.
- Diminui menos 1, devido o primeiro
- item começa com zero. 
- Se temos 2 items, sua posição:
- 0,1,2
-*/
-var total    = sliders.length - 1;
-/*
-  Executaremos uma função num intervalo de tempo.
-  E defiremos que executará em 2000 milesegundos
-*/
-window.setInterval(function(){
-	/*
-	 a variavel index será responsável pela
-	 posição do elemento que iremos remover a classe
-	*/
-	var index =  current ? current - 1 : total;
-	/*Pega o elemento pra remover a classe*/
-	sliders[index].className  = '';
-	/*Adiciona a classe no elemento atual*/
-	sliders[current].className  = 'slider-active';
-	/*Calcular a posição do próximo elemento que será exibido*/
-	current = current >= total ? 0 : current+1; 	
-}, 2000);
+// Só executa o código se realmente encontrar os elementos na página
+if (sliders.length > 0) {
+  var current = 0;
+  var total = sliders.length - 1;
+
+  window.setInterval(function () {
+    var index = current ? current - 1 : total;
+
+    // Garante que os elementos existem antes de mexer nas classes
+    if (sliders[index] && sliders[current]) {
+      sliders[index].className = "";
+      sliders[current].className = "slider-active";
+    }
+
+    current = current >= total ? 0 : current + 1;
+  }, 2000);
+}
